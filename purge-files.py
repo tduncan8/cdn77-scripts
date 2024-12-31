72146946d9350299cfae1b36c214a8e7dc34dc14
tduncan8 <tdunca7@gmail.com>
2024-12-31 15:10:11 -0500

    Improve argeparse

--- purge-files.py
+++ purge-files.py
@@ -1,9 +1,16 @@
 import argparse
 import requests
+import sys
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-z","--zone", help="cdn77 zone id")
-parser.add_argument("-t","--token", help="cdn77 api token")
-parser.add_argument("-f","--file", help="file to purge from cache")
+parser.add_argument("--zone", help="cdn77 zone id")
+parser.add_argument("--token", help="cdn77 api token")
+parser.add_argument("--file", help="file to purge from cache")
+
+# check for no arguments entered and print help and exit.
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(0)
+
 args = parser.parse_args()
 
