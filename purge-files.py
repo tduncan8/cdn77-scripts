0454ad6c70e0a5a0b83f8c90c9750d3ce4fa953f
tduncan8 <tdunca7@gmail.com>
2024-12-31 16:05:30 -0500

    Add error handling and json parse of output.

--- purge-files.py
+++ purge-files.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import sys
+import json
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--zone", help="cdn77 zone id", type=int, required=True)
@@ -20,13 +21,28 @@ data = {'paths' : file}
 headers = {'Authorization': f"Bearer {auth_token}"}
 url = f"https://api.cdn77.com/v3/cdn/{zone}/job/purge"
 
-print(f"- Interacting with zone url: {url}")
+print(f"Submitting payload to zone url: {url}")
 
 def purge_file(url, data, headers):
-  response = requests.post(url, json=data, headers=headers)
-  return response
+    try:
+      response = requests.post(url, json=data, headers=headers)
+      response.raise_for_status()
+      return response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
 
 
 purge_results = purge_file(url,data,headers)
-print(purge_results.json())
-
+if purge_results:
+    try:
+      output = purge_results.json()
+      print(f"Job Submission Results:")
+      print("ID:", output["id"])
+      print("State:", output["state"])
+      print("Started:", output["queued_at"])
+      print("Paths:", output["paths"])
+    except ValueError:
+        print("Error: The response could not be parsed as JSON.")
+else:
+ print('purge request failed')
